78810794eb087bee4fc63d57861aa8e13dc45507
Shialein <92607201+Shialein@users.noreply.github.com>
2022-01-16 15:47:08 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,62 @@
 import os
+import discord
+import logging
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+class TutorialBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+    # Greetings
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Eingeloggt als {self.bot.user} ({self.bot.user.id})')
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+    # Reconnect
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        print('Bot wieder da')
+
+    # Error Handlers
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        # Uncomment line 26 for printing debug
+        # await ctx.send(error)
+
+        # Unknown command
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send('WAs kannst du eigentlich? Das gibts nicht')
+
+        # Bot does not have permission
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send('Brauch mehr rechte sadge')
+
+
+# Gateway intents
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+
+# Bot prefix
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'),
+                   description='Nur Eskalativer Bot', intents=intents)
+
+# Logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+if __name__ == '__main__':
+    # Load extension
+    for filename in os.listdir('./commands'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'commands.{filename[: -3]}')
+
+    bot.add_cog(TutorialBot(bot))
+    bot.run(token, reconnect=True)
